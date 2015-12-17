8bcaeabe98bb2c860f99afbbc2c0e28a70161fa6
Lucas Dimas Cavalcante Lopes <lucasdcavalcante@gmail.com>
2015-12-17 11:19:47 -0200

    remocao de arquivo extra de conexao

--- Site simples em PHP/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-	try{
-		$conexao = new \PDO("mysql:host=localhost;dbname=pdo","root","root");
-	}catch(\PDOException $e) {
-		die("Erro de conexão com o banco de dados! Erro Código: ".$e->getCode().": ".$e->getMessage());
-	}
-
- ?>
